3574fea2437c2ee5198d79534fd9bc748059308a
Handika Rafli A <shandika231@gmail.com>
2022-09-19 17:33:50 +0700

    add lotie

--- lib/presentation/page/login_view.dart
+++ lib/presentation/page/login_view.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class LoginView extends StatelessWidget {
   static const String routeName = '/login';
   const LoginView({super.key});
-// https://assets5.lottiefiles.com/packages/lf20_mjlh3hcy.json
+//
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Column(
-            children: [
-              Expanded(
-                child: Container(
-                  color: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text(
+                'Welcome Back!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
               ),
-              Expanded(
-                child: Container(
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
-          Column()
-        ],
+            ),
+            Lottie.network(
+              'https://assets5.lottiefiles.com/packages/lf20_mjlh3hcy.json',
+              width: double.infinity,
+              height: 200,
+            ),
+            const TextField(),
+          ],
+        ),
       ),
     );
   }
